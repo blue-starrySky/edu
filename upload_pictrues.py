525764866990cc533ebfbabdfb23ab8a26a81257
blue <765072521@qq.com>
2019-05-27 18:08:14 +0800

    添加图片下载功能

--- upload_pictrues.py
+++ upload_pictrues.py
@@ -62,4 +62,4 @@ def index(imageid):
 if __name__ == '__main__':
 # app.debug = True
 
-    app.run(host='0.0.0.0', port=8987, debug=True)
+    app.run(host="0.0.0.0",port=5000,ssl_context=('2153883_xn--5vrwma.com.pem', '2153883_xn--5vrwma.com.key'),debug=True)
