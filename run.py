2cca63411c5a5acc5d1ada9b6acaaf3dc2503f40
blue <765072521@qq.com>
2019-06-03 18:48:03 +0800

    Merge branch 'master' of https://github.com/blue-starrySky/edu
    # Conflicts:
    #	run.py

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 # -*- coding:utf-8 -*
+import logging
+from wechatpy.utils import check_signature
 import hashlib
 from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify, Response
 from werkzeug.utils import secure_filename
 import os
 from datetime import timedelta
-
+logging.basicConfig(filename="./flask.log")
 # 设置允许的文件格式
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'bmp', 'jpeg'])
 
@@ -61,25 +63,28 @@ def index(imageid):
     resp = Response(image, mimetype="image/jpeg")
     return resp
 
-@app.route('/token',methods=['GET','POST'])
+
+@app.route('/wx_pub',methods=['GET','POST'])
 def wx():
-    if request.method == 'GET':
-        signature = request.args.get('signature')
-        timestamp = request.args.get('timestamp')
-        echostr = request.args.get('echostr')
-        nonce = request.args.get('nonce')
-        token = 'xiaofeishu'
-        if len(request.args)==0:
-            return "hello, this is handle view"
-        list = [token, timestamp, nonce]
-        list.sort()
-        s = list[0]+list[1]+list[2]
-        hashcode = hashlib.sha1(s.encode('utf-8')).hexdigest()
-        if hashcode == signature:
-            return echostr
-        else:
-            print('验证失败')
-            return ""
+    signature = request.args.get('signature')
+    timestamp = request.args.get('timestamp')
+    nonce = request.args.get('nonce')
+    echostr = request.args.get('echostr')
+    token = "xiaofeishu"
+    list = [token, timestamp, nonce]
+    list.sort()
+    sha1 = hashlib.sha1()
+    sha1.update(list[0].encode('utf-8'))
+    sha1.update(list[1].encode('utf-8'))
+    sha1.update(list[2].encode('utf-8'))
+    hashcode = sha1.hexdigest()
+    logging.info("handle/GET func: hashcode, signature, timestamp, nonce, echostr, token: ", hashcode, signature, timestamp,
+          nonce, echostr)
+    if hashcode == signature:
+        return echostr
+    else:
+        logging.warning("hashcode != signature")
+        return ""
 
 #app.run(host="127.0.0,1",port=5555)
 if __name__ == '__main__':
